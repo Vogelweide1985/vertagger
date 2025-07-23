145ed697532e3f22a30c08bc7aeea20aac3d6e7f
vogelweide1985 <vogelweide1985@hotmail.de>
2025-07-23 09:34:19 +0200

    Integration von comet opik

--- src/vertagger/config.py
+++ src/vertagger/config.py
@@ -4,7 +4,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class GlobalSettings(BaseSettings):
     # Sie lädt nur den einen Key, den die App zum Starten braucht
     openai_api_key: str
-
+    opik_api_key: str 
+    opik_workspace: str 
+    opik_project_name: str 
     model_config = SettingsConfigDict(env_file=".env")
 
 # Wir erstellen eine Instanz, die wir in main.py importieren können
--- src/vertagger/main.py
+++ src/vertagger/main.py
@@ -1,14 +1,21 @@
+import os
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from openai import AsyncOpenAI
 from .__about__ import __version__
-from .config import global_settings # Dein globaler API Key
+from .config import global_settings 
 from .api.v1_0 import endpoints as v1_endpoints
+
+from opik.integrations.openai import track_openai
 
 #Der Client wird hier einmalig erstellt und in der App gespeichert
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    app.state.openai_client = AsyncOpenAI(api_key=global_settings.openai_api_key)
+
+    os.environ["OPIK_API_KEY"] = global_settings.opik_api_key
+    os.environ["OPIK_WORKSPACE"] = global_settings.opik_workspace
+    os.environ["OPIK_PROJECT_NAME"] = global_settings.opik_project_name
+    app.state.openai_client = track_openai(AsyncOpenAI(api_key=global_settings.openai_api_key))
     yield
     await app.state.openai_client.close()
 
